a0596c1af7a0229fc98cc24cd87c66b5df0e358c
Mayank <mayankagarwal997@gmail.com>
2019-06-04 15:48:42 +0530

    Login/Register method added with Logout Option

---
+++ app/src/main/java/com/example/mayankagarwal/dblog/StartActivity.java
@@ -0,0 +1,40 @@
+package com.example.mayankagarwal.dblog;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+    private Button register,login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        register = (Button) findViewById(R.id.register_button);
+        login = (Button) findViewById(R.id.login_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(StartActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+                finish();
+            }
+        });
+
+    }
+
+
+}
